28431a4fb9874ae95be3276c76896870e03fc6ef
unknown <59000491+sinarmedcon@users.noreply.github.com>
2021-03-24 16:42:31 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/silsilah/';
+$protocol = '';
+if (isset($_SERVER['HTTPS']) &&
+    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $protocol = 'https://';
+}
+else {
+    $protocol = 'http://';
+}
+
+$config['base_url'] = $protocol.$_SERVER['HTTP_HOST'].'/';
 
 /*
 |--------------------------------------------------------------------------
