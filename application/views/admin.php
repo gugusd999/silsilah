24c3098fba7dc163f075c3565edaac470d904c73
unknown <59000491+sinarmedcon@users.noreply.github.com>
2021-03-25 16:59:21 +0700

    update silsilah

--- application/views/admin.php
+++ application/views/admin.php
@@ -13,3 +13,25 @@
          </div>
      </div>
  </div>
+<br>
+<h2 style="text-align: center !important;">Silsilah Keluarga Besarku</h2>
+<div class="notika-status-area">
+     <div class="container">
+         <div class="row">
+             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                 <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
+                     <div class="website-traffic-ctn">
+                         <p>Total keluarga</p>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                 <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
+                     <div class="website-traffic-ctn">
+                         <p>Total keluarga</p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
